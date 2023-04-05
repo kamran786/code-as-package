e743e0786cafa256fe7d439149a0fe7f3710ffa3
Kamran Ahmad <kahmad595@gmail.com>
2023-04-05 00:01:52 -0400

    testing

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='my_code_as_package',
     version='0.1',
@@ -7,10 +11,10 @@ setup(
     author='Kamran Ahmad',
     author_email='kahmad595@gmail.com',
     packages=find_packages(),
-    install_requires=requirements.txt,
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'my_code_as_pkg=src.__main__.:main'
+            'my_code_as_pkg=src.__main__:main'
         ]
     },
 )
