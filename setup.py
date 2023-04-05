5014b28e6dcfae0637ceeb27026844e0bd0e66e5
Kamran Ahmad <kahmad595@gmail.com>
2023-04-05 00:45:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='my_code_as_package',
-    version='0.4',
+    version='0.5',
     description='My awesome package',
     author='Kamran Ahmad',
     author_email='kahmad595@gmail.com',
@@ -14,7 +14,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'my_code_as_pkg=src.__main__:__main__'
+            'my_code_as_pkg=src.__main__:main'
         ]
     },
 )
